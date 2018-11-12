cd909d466fc648979f4ca34a46e5943b62e7431e
root <th3J0kr@protonmail.com>
2018-11-12 16:49:58 -0500

    Support for debian and ubuntu servers

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-Scripts and accompying configs for server build
+# Scripts and accompying configs for server build
+
+#### _Currently still very early in development_
+
+#### _Author: Th3J0kr_
+
+#### _Version: 0.1_
+
+##Usage
+
+All config files are stored in <type of machine>/config/
+
+### Servers
+
+Currently only supports Debian based servers (debian and ubuntu)
+
+Configures everything as if the root user is the primary user (Yes insecure but it's how I roll)
+
+Goes through and downloads the packages I use the most on servers `zsh vim glances python3 php apache2 tmux curl wget git openssh-server`
+
+Then it configures: 
+
+* zsh using oh-my-zsh and basic custom `zshrc` file
+* tmux using oh-my-tmux and basic `tmux.conf.local` file
+* SSH server to allow root login with password (Yes it's insecure, adding key management soon)
+
+### Workstations
+
+Coming soon...
+
+## Additional tid bits
+
+Feel free to add your own config files to the `config` directory just make sure to keep the same structure
--- server/debian.sh
+++ server/debian.sh
@@ -1,26 +1,94 @@
 #!/bin/bash
 
-#Verify on debian
-if [ -f /etc/debian_version ]; then
-    echo "[*] Confirmed on debain, verifying users..."
 
-    user=$( whoami )
-    echo "[*] Running as user $user"
+installOhMyZSH() {
+    echo "[*] Installing Oh My ZSH..."
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    echo "[*] Finished isntalling oh-my-zsh, configuring..."
+    if [ -f 'config/.zshrc' ]; then
+        if [ -f '/root/.zshrc' ]; then
+            mv '/root/.zshrc' '/root/.zshrc.bak'
+        fi
+        cp 'config/.zshrc' '/root/.zshrc'
+        echo "[*] Done configuring zshrc!"
+    else
+        echo "[!] No config file provided, leaving defaults"
+    fi
+}
 
-    if [ $user = 'root' ]; then
-        echo "[*] Installing packages..."
+configureTmux() {
+    echo "[*] Configuring tmux with oh-my-tmux"
+    git clone https://github.com/gpakosz/.tmux.git '/root/.tmux'
+    if [ -d 'config/.tmux' ]; then
+        if [ -d '/root/.tmux' ]; then
+            rm -rf '/root/.tmux'
+        fi
+        cp -r 'config/.tmux' '/root/.tmux'
+        ln -s -f '/root/.tmux/.tmux.conf'
+        cp '/root/.tmux/.tmux.conf.local' '/root/'
+        echo "[*] Done configuring tmux!"
+    else
+        echo "[!] No config file provided, leaving defaults"
+    fi
+}
 
-        apt update
+configureSSH() {
+    echo "[*] Configuring SSH server"
+    if [ -f 'config/sshd_config' ]; then
+        if [ -f '/etc/ssh/sshd_config' ]; then
+            mv '/etc/ssh/sshd_config' '/etc/ssh/sshd_config.bak'
+            cp 'config/sshd_config' '/etc/ssh/sshd_config'
+            echo "[*] Restarting SSH server..."
+            systemctl restart sshd
+        else
+            echo "[!] No sshd_config file, check openssh-server installation!"
+        fi
+        echo "[*] Done configuring SSH server!"
+    else
+        echo "[!] No config file provided, leaving defaults"
+    fi
+}
 
-        apt install zsh vim glances python3 php apache2 tmux curl wget git 
+main() {
+    #Verify on debian
+    if [ -f /etc/debian_version ]; then
+        echo "[*] Confirmed on debain, verifying users..."
+
+        user=$( whoami )
+        echo "[*] Running as user $user"
+
+        if [ $user = 'root' ]; then
+            echo "[*] Installing packages..."
+
+            apt update
+            apt install zsh vim glances python3 php apache2 tmux curl wget git openssh-server
+        
+        else
+            echo "[!] This script needs to be run as root, quitting!"
+        fi
+
+    elif type lsb_release >/dev/null 2>&1; then
+        os=$( lsb_relase -si )
+        if [ $os = 'Ubuntu' ]; then
+            echo "[*] Confirmed on debain, verifying users..."
+
+            user=$( whoami )
+            echo "[*] Running as user $user"
+
+            if [ $user = 'root' ]; then
+                echo "[*] Installing packages..."
+
+                apt update
+                apt install zsh vim glances python3 php apache2 tmux curl wget git openssh-server
+            
+            else
+                echo "[!] This script needs to be run as root, quitting!"
+            fi
+        fi
     
     else
-        echo "[!] Not running as root, quitting!"
-
+        echo "[!] Not on Debian based system, quitting!"
     fi
-
-    else
-        echo "[!] Not on Debian, quitting!"
-
-
-fi
+}
+main "$@"
+exit 0
